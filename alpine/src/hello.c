9c267e58ce1daf34682bf46b0ecd380065970e51
accetto <34798830+accetto@users.noreply.github.com>
2021-04-16 20:00:16 +0200

    alpine image updated

--- alpine/src/hello.c
+++ alpine/src/hello.c
@@ -2,7 +2,7 @@
 
 /*
 HOWTO in Alpine: 
-1) install gcc as 'apk add --no-cache build-base'
+1) install gcc as 'apk add build-base'
 2) compile it as 'gcc hello.c -o hello'
 3) run it as './hello'
 */
