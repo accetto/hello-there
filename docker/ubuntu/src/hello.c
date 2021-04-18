a2babf0777a3a48cafd806c4b735595a9a42cfc1
accetto <34798830+accetto@users.noreply.github.com>
2021-04-18 17:17:07 +0200

    ubuntu image added, build hook improved

---
+++ docker/ubuntu/src/hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+HOWTO in Ubuntu: 
+1) install gcc as 'apt-get install --no-install-recommends gcc libc6-dev'
+2) compile it as 'gcc hello.c -o hello'
+3) run it as './hello'
+*/
+
+int main() {
+    puts("Hello there from Ubuntu!");
+    return 0;
+}
